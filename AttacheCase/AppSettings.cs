eb0ae016203459402bd21ec4a081ef99963aaf15
Hibara, Mitsuhiro <m@hibara.org>
2019-06-13 17:15:28 +0900

    Fix the problem where the text file containing the file list to be processed specified in the command line option "/ list" at startup can not be read correctly if it has a character encoding other than UTF-8

--- AttacheCase/AppSettings.cs
+++ AttacheCase/AppSettings.cs
@@ -2764,26 +2764,77 @@ namespace AttacheCase
             break;
 
           case "/list": // リストファイルからのパスの読み込み
-            string dir = Path.GetDirectoryName(value);
             if (File.Exists(value) == true)
             {
               int c = 0;
+              string dir = Path.GetDirectoryName(value);
               string line;
+
+              // Detect text encoding
+              byte[] bs = File.ReadAllBytes(value);
+              //文字コードを取得する
+              Encoding enc = DetectEncoding(bs);
+
+              if (enc == null)
+              {
+                // 注意
+                // 
+                // 以下の指定されたリストファイルの文字エンコードが不明で読み取れません！
+                // [FileListPath]
+                //
+                // Alert
+                // The character encoding of the following specified list file is unknown that can not be read!
+                // [FileListPath]
+                MessageBox.Show(Resources.DialogMessageFileListEncodingUnknown + "\n" + value,
+                Resources.DialogTitleAlert, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return (-1);
+              }
+
               // Read the List file and Add to FileList.
-              using (StreamReader sr = new StreamReader(value))
+              using (StreamReader sr = new StreamReader(value, enc))
               {
                 while ((line = sr.ReadLine()) != null)
                 {
                   if (line == "") continue;
                   string filename = Path.Combine(dir, line);
                   string fullpath = Path.GetFullPath(filename);
-                  if ( File.Exists(fullpath))
+                  if ( File.Exists(fullpath) == true)
                   {
                     _FileList.Add(fullpath);
+                  }
+                  else
+                  {
+                    // 注意
+                    // 
+                    // 以下の指定されたファイルリストの中のファイルが見つかりません！
+                    // [FileListPath]
+                    // [FilePath]
+                    //
+                    // Alert
+                    // The file in the following specified file's list can not be found!
+                    // [FileListPath]
+                    // [FilePath]
+                    MessageBox.Show(Resources.DialogMessageFileInFileListNotFound + "\n/list=" + value + "\n" + fullpath,
+                    Resources.DialogTitleAlert, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return (-1);
                   }
                   c++;
                 }
               }
+            }
+            else
+            {
+              // 注意
+              // 
+              // 以下の指定されたリストファイルが見つかりません！
+              // [FilePath]
+              //
+              // Alert
+              // The following specified list file can not be found!
+              // [FilePath]
+              MessageBox.Show(Resources.DialogMessageFileListNotFound + "\n" + value,
+              Resources.DialogTitleAlert, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+              return (-1);
             }
             break;
 
@@ -3478,19 +3529,217 @@ namespace AttacheCase
         }
 
       }
-
       //string text = System.Text.Encoding.UTF8.GetString(result);
       string text = Encoding.GetEncoding("shift_jis").GetString(result);
 
       return (result);
-
     }
 
-    
+    //======================================================================
+    /// <summary>
+    /// 文字コードを判別する(Detect Encoding)
+    /// </summary>
+    /// <remarks>
+    /// 参考：http://dobon.net/vb/dotnet/string/detectcode.html
+    /// Jcode.pmのgetcodeメソッドを移植したものです。
+    /// Jcode.pm(http://openlab.ring.gr.jp/Jcode/index-j.html)
+    /// Jcode.pmのCopyright: Copyright 1999-2005 Dan Kogai
+    /// </remarks>
+    /// <param name="bytes">文字コードを調べるデータ</param>
+    /// <returns>適当と思われるEncodingオブジェクト。
+    /// 判断できなかった時はnull。</returns>
+    //======================================================================
+    public static Encoding DetectEncoding(byte[] bytes)
+    {
+      const byte bEscape = 0x1B;
+      const byte bAt = 0x40;
+      const byte bDollar = 0x24;
+      const byte bAnd = 0x26;
+      const byte bOpen = 0x28;    //'('
+      const byte bB = 0x42;
+      const byte bD = 0x44;
+      const byte bJ = 0x4A;
+      const byte bI = 0x49;
+
+      int len = bytes.Length;
+      byte b1, b2, b3, b4;
+
+      //Encode::is_utf8 は無視
+
+      bool isBinary = false;
+      for (int i = 0; i < len; i++)
+      {
+        b1 = bytes[i];
+        if (b1 <= 0x06 || b1 == 0x7F || b1 == 0xFF)
+        {
+          //'binary'
+          isBinary = true;
+          if (b1 == 0x00 && i < len - 1 && bytes[i + 1] <= 0x7F)
+          {
+            //smells like raw unicode
+            return System.Text.Encoding.Unicode;
+          }
+        }
+      }
+      if (isBinary)
+      {
+        return null;
+      }
+
+      //not Japanese
+      bool notJapanese = true;
+      for (int i = 0; i < len; i++)
+      {
+        b1 = bytes[i];
+        if (b1 == bEscape || 0x80 <= b1)
+        {
+          notJapanese = false;
+          break;
+        }
+      }
+      if (notJapanese)
+      {
+        return System.Text.Encoding.ASCII;
+      }
+
+      for (int i = 0; i < len - 2; i++)
+      {
+        b1 = bytes[i];
+        b2 = bytes[i + 1];
+        b3 = bytes[i + 2];
+
+        if (b1 == bEscape)
+        {
+          if (b2 == bDollar && b3 == bAt)
+          {
+            //JIS_0208 1978
+            //JIS
+            return System.Text.Encoding.GetEncoding(50220);
+          }
+          else if (b2 == bDollar && b3 == bB)
+          {
+            //JIS_0208 1983
+            //JIS
+            return System.Text.Encoding.GetEncoding(50220);
+          }
+          else if (b2 == bOpen && (b3 == bB || b3 == bJ))
+          {
+            //JIS_ASC
+            //JIS
+            return System.Text.Encoding.GetEncoding(50220);
+          }
+          else if (b2 == bOpen && b3 == bI)
+          {
+            //JIS_KANA
+            //JIS
+            return System.Text.Encoding.GetEncoding(50220);
+          }
+          if (i < len - 3)
+          {
+            b4 = bytes[i + 3];
+            if (b2 == bDollar && b3 == bOpen && b4 == bD)
+            {
+              //JIS_0212
+              //JIS
+              return System.Text.Encoding.GetEncoding(50220);
+            }
+            if (i < len - 5 &&
+                b2 == bAnd && b3 == bAt && b4 == bEscape &&
+                bytes[i + 4] == bDollar && bytes[i + 5] == bB)
+            {
+              //JIS_0208 1990
+              //JIS
+              return System.Text.Encoding.GetEncoding(50220);
+            }
+          }
+        }
+      }
+
+      //should be euc|sjis|utf8
+      //use of (?:) by Hiroki Ohzaki <ohzaki@iod.ricoh.co.jp>
+      int sjis = 0;
+      int euc = 0;
+      int utf8 = 0;
+      for (int i = 0; i < len - 1; i++)
+      {
+        b1 = bytes[i];
+        b2 = bytes[i + 1];
+        if (((0x81 <= b1 && b1 <= 0x9F) || (0xE0 <= b1 && b1 <= 0xFC)) &&
+            ((0x40 <= b2 && b2 <= 0x7E) || (0x80 <= b2 && b2 <= 0xFC)))
+        {
+          //SJIS_C
+          sjis += 2;
+          i++;
+        }
+      }
+      for (int i = 0; i < len - 1; i++)
+      {
+        b1 = bytes[i];
+        b2 = bytes[i + 1];
+        if (((0xA1 <= b1 && b1 <= 0xFE) && (0xA1 <= b2 && b2 <= 0xFE)) ||
+            (b1 == 0x8E && (0xA1 <= b2 && b2 <= 0xDF)))
+        {
+          //EUC_C
+          //EUC_KANA
+          euc += 2;
+          i++;
+        }
+        else if (i < len - 2)
+        {
+          b3 = bytes[i + 2];
+          if (b1 == 0x8F && (0xA1 <= b2 && b2 <= 0xFE) &&
+              (0xA1 <= b3 && b3 <= 0xFE))
+          {
+            //EUC_0212
+            euc += 3;
+            i += 2;
+          }
+        }
+      }
+      for (int i = 0; i < len - 1; i++)
+      {
+        b1 = bytes[i];
+        b2 = bytes[i + 1];
+        if ((0xC0 <= b1 && b1 <= 0xDF) && (0x80 <= b2 && b2 <= 0xBF))
+        {
+          //UTF8
+          utf8 += 2;
+          i++;
+        }
+        else if (i < len - 2)
+        {
+          b3 = bytes[i + 2];
+          if ((0xE0 <= b1 && b1 <= 0xEF) && (0x80 <= b2 && b2 <= 0xBF) &&
+              (0x80 <= b3 && b3 <= 0xBF))
+          {
+            //UTF8
+            utf8 += 3;
+            i += 2;
+          }
+        }
+      }
+      //M. Takahashi's suggestion
+      //utf8 += utf8 / 2;
+
+      //System.Diagnostics.Debug.WriteLine(string.Format("sjis = {0}, euc = {1}, utf8 = {2}", sjis, euc, utf8));
+      if (euc > sjis && euc > utf8)
+      {
+        //EUC
+        return System.Text.Encoding.GetEncoding(51932);
+      }
+      else if (sjis > euc && sjis > utf8)
+      {
+        //SJIS
+        return System.Text.Encoding.GetEncoding(932);
+      }
+      else if (utf8 > euc && utf8 > sjis)
+      {
+        //UTF8
+        return System.Text.Encoding.UTF8;
+      }
+      return null;
+    }
+       
   }
 
-
-
 }
-
-
--- AttacheCase/Properties/Resources.Designer.cs
+++ AttacheCase/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace AttacheCase.Properties {
     // または Visual Studio のようなツールを使用して自動生成されました。
     // メンバーを追加または削除するには、.ResX ファイルを編集して、/str オプションと共に
     // ResGen を実行し直すか、または VS プロジェクトをビルドし直します。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -286,6 +286,33 @@ namespace AttacheCase.Properties {
         public static string DialogMessageFileIndexInvalid {
             get {
                 return ResourceManager.GetString("DialogMessageFileIndexInvalid", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   The file in the following specified file&apos;s list can not be found! に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string DialogMessageFileInFileListNotFound {
+            get {
+                return ResourceManager.GetString("DialogMessageFileInFileListNotFound", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   The character encoding of the following specified list file is unknown that can not be read! に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string DialogMessageFileListEncodingUnknown {
+            get {
+                return ResourceManager.GetString("DialogMessageFileListEncodingUnknown", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   The following specified list file can not be found! に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        public static string DialogMessageFileListNotFound {
+            get {
+                return ResourceManager.GetString("DialogMessageFileListNotFound", resourceCulture);
             }
         }
         
